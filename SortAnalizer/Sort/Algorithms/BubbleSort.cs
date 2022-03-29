f836fda645a56e96db00817df3e66a58b00e58de
galoperidol08@gmail.com <galoperidol08@gmail.com>
2022-03-29 13:15:17 +1000

    add: BubbleSort

---
+++ SortAnalizer/Sort/Algorithms/BubbleSort.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort.Algorithms
+{
+    internal class BubbleSort<T> : IDiagnosticSortAlgorithm<T> where T : IComparable
+    {
+        public BubbleSort()
+        {
+        }
+
+        public int CompareCount { get ; private set; }
+        public int SwapCount { get ; private set; }
+
+        public event IDiagnosticSortAlgorithm<T>.Compare CompareTwo;
+        public event IDiagnosticSortAlgorithm<T>.Swap SwapTwo;
+
+        public IEnumerable<T> Sort(IEnumerable<T> array)
+        {
+            List<T> arrayList = array.ToList();
+
+            for (int i = 0; i < arrayList.Count; i++)
+            {
+                for (int j = i + 1; j < arrayList.Count; j++)
+                {
+                    CompareCount++;
+                    CompareTwo?.Invoke(i, j);
+                    
+                    if (arrayList[i].CompareTo(arrayList[j]) > 0)
+                    {
+                        SwapCount++;
+                        SwapTwo?.Invoke(i, j);
+
+                        var temp = arrayList[i];
+                        arrayList[i] = arrayList[j];
+                        arrayList[j] = temp;
+                    }
+                }
+            }
+
+            return arrayList;
+        }
+    }
+}
