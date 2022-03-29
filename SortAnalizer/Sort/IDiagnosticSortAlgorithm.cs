602c7c4dee4ffb4ba8325fe1745b861afe70a64e
galoperidol08@gmail.com <galoperidol08@gmail.com>
2022-03-29 12:44:10 +1000

    add: IDiagnosticSortAlgorithm

---
+++ SortAnalizer/Sort/IDiagnosticSortAlgorithm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort
+{
+    internal interface IDiagnosticSortAlgorithm<T> : ISortAlgorithm<T> where T : IComparable 
+    {
+        delegate (int, int) Compare();
+        public event Compare CompareTwo;
+
+        delegate (int, int) Swap();
+        public event Swap SwapTwo;
+    }
+}
