5aa2d83fab4180bca0bb48a3ba077a390bebc5e4
galoperidol08@gmail.com <galoperidol08@gmail.com>
2022-04-06 00:13:25 +1000

    remove using

--- WhiteSoftTask/Controller/MainController.cs
+++ WhiteSoftTask/Controller/MainController.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Utils.Json;
+﻿using Utils.Json;
 using Utils.Web;
 using WhiteSoftTask.Model;
-using WhiteSoftTask.View;
 
 namespace WhiteSoftTask.Controller
 {
